eac52e99d4c2a4e7e2f8f20c0d8363c6261f69d7
harshul <harshulgarg21@gmail.com>
2022-06-15 13:23:16 +0530

    Created new file

---
+++ app.py
@@ -0,0 +1,8 @@
+from instabot import Bot
+
+bot= Bot()
+
+bot.login(username="instagramsample123", password="Instagram890");
+image= "img/natrue.jpeg"
+
+bot.upload_photo(image,caption="TEST 111111111");
